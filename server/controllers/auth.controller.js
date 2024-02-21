e7ff77a85eeb0bbd49936c3deb46e5694546263d
Ivan Butakov <ivanbutakov000@gmail.com>
2024-02-21 20:12:27 +0500

    create sign in api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,24 +1,21 @@
 const User = require('../models/user.model')
 const bcryptjs = require('bcryptjs')
 const { validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken')
+const { errorHandler } = require('../utils/error')
 
 const signup = async (req, res, next) => {
 	try {
 		const errors = validationResult(req)
 		if (!errors.isEmpty()) {
-			console.log(errors)
-			return res
-				.status(400)
-				.json({ success: false, message: errors.array()[0].msg })
+			return next(errorHandler(400, errors.array()[0].msg))
 		}
 
 		const { username, email, password } = req.body
 
 		const candidate = await User.findOne({ email })
 		if (candidate) {
-			return res
-				.status(400)
-				.json({ success: false, message: 'User already exists!' })
+			return next(errorHandler(400, 'User already exists!'))
 		}
 
 		const hashedPassword = bcryptjs.hashSync(password, 7)
@@ -32,4 +29,26 @@ const signup = async (req, res, next) => {
 	}
 }
 
+const signin = async (req, res, next) => {
+	try {
+		const { email, password } = req.body
+		const user = await User.findOne({ email })
+		if (!user) {
+			return next(errorHandler(404, 'User not found!'))
+		}
+		const validPassword = bcryptjs.compareSync(password, user.password)
+		if (!validPassword) {
+			return next(errorHandler(400, 'Wrong password!'))
+		}
+		const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+			expiresIn: '24h',
+		})
+		const { password: pass, ...rest } = user._doc
+		res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest)
+	} catch (error) {
+		next(error)
+	}
+}
+
 module.exports.signup = signup
+module.exports.signin = signin
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,20 +1,20 @@
 const express = require('express')
-const { signup } = require('../controllers/auth.controller.js')
+const { signup, signin } = require('../controllers/auth.controller.js')
 const { check } = require('express-validator')
 
 const router = express.Router()
 
-router
-	.route('/signup')
-	.post(
-		[
-			check('username', 'Username is required ').notEmpty(),
-			check('email', 'Email is required').notEmpty(),
-			check('password', 'Password should be at least 4 characters').isLength({
-				min: 4,
-			}),
-		],
-		signup
-	)
+router.route('/signup').post(
+	[
+		check('username', 'Username is required ').notEmpty(),
+		check('email', 'Email is required').notEmpty(),
+		check('password', 'Password should be at least 4 characters').isLength({
+			min: 4,
+		}),
+	],
+	signup
+)
+
+router.route('/signin').post(signin)
 
 module.exports = router
