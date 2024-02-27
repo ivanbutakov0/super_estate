efce7ab95834d71cf9653af671d6b1cfaa00d80e
Ivan Butakov <ivanbutakov000@gmail.com>
2024-02-27 19:06:45 +0500

    create delete use api

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -44,5 +44,23 @@ const updateUser = async (req, res, next) => {
 	}
 }
 
+const deleteUser = async (req, res, next) => {
+	if (req.user.id !== req.params.id) {
+		return next(errorHandler(403, 'You can only delete your own profile!'))
+	}
+
+	try {
+		await User.findByIdAndDelete(req.params.id)
+
+		res.status(200).json({
+			success: true,
+			message: 'User has been deleted successfully!',
+		})
+	} catch (error) {
+		next(error)
+	}
+}
+
 module.exports.sayHello = sayHello
 module.exports.updateUser = updateUser
+module.exports.deleteUser = deleteUser
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,15 @@
 const express = require('express')
-const { sayHello, updateUser } = require('../controllers/user.controller.js')
+const {
+	sayHello,
+	updateUser,
+	deleteUser,
+} = require('../controllers/user.controller.js')
 const verifyToken = require('../utils/verifyUser.js')
 
 const router = express.Router()
 
 router.route('/').get(sayHello)
 router.use(verifyToken).route('/update/:id').put(updateUser)
+router.use(verifyToken).route('/delete/:id').delete(deleteUser)
 
 module.exports = router
